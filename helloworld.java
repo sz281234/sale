4b4de173a76b2681815f8d277cf03298657df4de
passi <kvps012@gmail.com>
2020-11-29 18:47:48 +0000

    changes made

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 //this is a new edit
 public class helloworld {
 	public static void main(String[] args) {
-		system.out.println("hello, world");
+	system.out.println("hello, world");
+	system.out.print("hello, world");
 	}
 }
 
