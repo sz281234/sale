31151503427a21c8094bdea57f355d55038051d4
passi <kvps012@gmail.com>
2020-11-30 16:44:25 +0000

    changed world to git

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 public class helloworld {
 	public static void main(String[] args) {
 	system.out.println("hello, world");
-	system.out.print("hello, world");
+	system.out.print("hello, git");
 	}
 }
 
